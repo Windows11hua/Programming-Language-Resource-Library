ba9837f50932a451deb7ca1b70e071c3c9db8852
MShh <152751310+Windows11hua@users.noreply.github.com>
2024-07-28 17:18:45 +0800

    Add files via upload

---
+++ 友元/友元函数.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+class Point
+{
+public:
+	Point(int xx = 0, int yy = 0);
+	int getX();
+	int getY();
+	friend double getdistance(Point start, Point end);
+private:
+	int x, y;
+};
+Point::Point(int xx, int yy)
+{
+	x = xx;
+	y = yy;
+}
+int Point::getX()
+{
+	return x;
+}
+int Point::getY()
+{
+	return y;
+}
+double getdistance(Point start, Point end)
+{
+	double d;
+	d = sqrt((end.x - start.x) * (end.x - start.x) + (end.y - start.y) + (end.y - start.y));
+	return d;
+}
+int main()
+{
+	Point p1(1, 1), p2(4, 5);
+	double d;
+	d = getdistance(p1, p2);
+	cout << "两点之间的距离为：" << d;
+	return 0;
+}
