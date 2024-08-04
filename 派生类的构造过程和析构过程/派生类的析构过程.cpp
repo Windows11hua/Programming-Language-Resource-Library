dfd0dd37a72539bb3c2c31a492edb242063304fc
MShh <152751310+Windows11hua@users.noreply.github.com>
2024-08-04 17:32:03 +0800

    Add files via upload

---
+++ 派生类的构造过程和析构过程/派生类的析构过程.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+class A
+{
+	int a;
+public:
+	A(int x) :a(x)
+	{
+		cout << "构建 A " << a << endl;
+	}
+	~A()
+	{
+		cout << "销毁 A " << a << endl;
+	}
+};
+class B :public A
+{
+private:
+	int b, c;
+	const int d;
+	A x, y;
+public:
+	B(int v) :b(v), y(b + 2), x(b + 1), d(b), A(v)
+	{
+		c = v;
+		cout << "构建 B " << b << " " << c << " " << d << endl;
+	}
+	~B()
+	{
+		cout << "销毁 B " << b << " " << c << " " << d << endl;
+	}
+};
+int main()
+{
+	B b1(10);
+	return 0;
+}
